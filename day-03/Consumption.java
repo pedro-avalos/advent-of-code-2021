3b75de439463a61a39cb800adb9ef49388410d4b
Pedro Avalos <pavalos6401@gmail.com>
2021-12-03 22:56:41 -0600

    day 3 part 2

--- day-03/Consumption.java
+++ day-03/Consumption.java
@@ -18,7 +18,7 @@ public class Consumption {
     try {
       scanner = new Scanner(file);
     } catch (FileNotFoundException e) {
-      System.out.println("File " + name + " not found.");
+      System.out.println("File '" + name + "' not found.");
       e.printStackTrace();
       System.exit(1);
     }
@@ -32,11 +32,11 @@ public class Consumption {
     String gammaBits = "";
     String epsilonBits = "";
     for (int i = 0; i < length; i++) {
-      int count = 0; // Count of 1s in the current position
+      int countOnes = 0;
 
       for (String num : report) {
         try {
-          count += Integer.parseInt("" + num.charAt(i));
+          countOnes += Integer.parseInt("" + num.charAt(i));
         } catch (NumberFormatException e) {
           System.out.println("Noninteger input.");
           e.printStackTrace();
@@ -44,15 +44,48 @@ public class Consumption {
         }
       }
 
-      gammaBits += 2 * count > report.size() ? 1 : 0;
-      epsilonBits += 2 * count < report.size() ? 1 : 0;
+      gammaBits += (2 * countOnes >= report.size()) ? 1 : 0;
+      epsilonBits += (2 * countOnes < report.size()) ? 1 : 0;
     }
 
     int gamma = Integer.parseInt(gammaBits, 2);
     int epsilon = Integer.parseInt(epsilonBits, 2);
 
-    System.out.println(gamma);
-    System.out.println(epsilon);
-    System.out.println(gamma * epsilon);
+    System.out.println("Part 1: " + (gamma * epsilon));
+
+    ArrayList<String> generatorCandidates = new ArrayList<>(report);
+    int i = 0;  // Position of char being considered
+    while (generatorCandidates.size() > 1) {
+      int countOnes = 0;
+      for (String s : generatorCandidates)
+        countOnes += Integer.parseInt("" + s.charAt(i));
+      char majority = (2 * countOnes >= generatorCandidates.size()) ? '1' : '0';
+
+      for (int j = 0; j < generatorCandidates.size(); j++)
+        if (generatorCandidates.get(j).charAt(i) != majority)
+          generatorCandidates.remove(j--);
+
+      i++;
+    }
+
+    ArrayList<String> scrubberCandidates = new ArrayList<>(report);
+    i = 0;  // Position of char being considered
+    while (scrubberCandidates.size() > 1) {
+      int countOnes = 0;
+      for (String s : scrubberCandidates)
+        countOnes += Integer.parseInt("" + s.charAt(i));
+      char minority = (2 * countOnes < scrubberCandidates.size()) ? '1' : '0';
+
+      for (int j = 0; j < scrubberCandidates.size(); j++)
+        if (scrubberCandidates.get(j).charAt(i) != minority)
+          scrubberCandidates.remove(j--);
+
+      i++;
+    }
+
+    long generator = Integer.parseInt(generatorCandidates.get(0), 2);
+    long scrubber = Integer.parseInt(scrubberCandidates.get(0), 2);
+
+    System.out.println("Part 2: " + (generator * scrubber));
   }
 }
