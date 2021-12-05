4dc5f4148855eb2d5a0a8ba2135364bf24c429de
Pedro Avalos <pavalos6401@gmail.com>
2021-12-04 18:01:14 -0600

    day 4 edits

--- day-04/bingo.py
+++ day-04/bingo.py
@@ -18,7 +18,7 @@ import numpy as np
 
 def get_input(file_name: str) -> tuple[list[int], list[np.ndarray]]:
     """
-    Retrive the numbers called and the boards.
+    Retrieve the numbers called and the boards.
 
     Args:
         file_name (str): Name of the file with the input data.
@@ -29,10 +29,31 @@ def get_input(file_name: str) -> tuple[list[int], list[np.ndarray]]:
 
     with open(file_name) as f:
         order = list(map(int, f.readline().strip().split(",")))
-        nums: list[int] = list(map(int, f.read().replace("\n", " ").strip().split()))
-        tmp: list[list[int]] = [nums[i : i + 5] for i in range(0, len(nums), 5)]
+        board_nums: list[int] = list(
+            map(int, f.read().replace("\n", " ").strip().split())
+        )
+        tmp: list[list[int]] = [
+            board_nums[i : i + 5] for i in range(0, len(board_nums), 5)
+        ]
         boards = [np.array(tmp[i : i + 5]) for i in range(0, len(tmp), 5)]
     return order, boards
+
+
+def mark(board: np.ndarray, num: int) -> None:
+    """
+    Check if the board contains the current number.
+
+    If the board contains the number, then "mark" it by setting the cell to -1.
+
+    Args:
+        board (np.ndarray): Board to be checked.
+        num (int): Number called out.
+    """
+
+    for i, row in enumerate(board):
+        for j, val in enumerate(row):
+            if val == num:
+                board[i][j] = -1
 
 
 def has_won(board: np.ndarray) -> bool:
@@ -87,35 +108,38 @@ def main() -> None:
         print("File '{file_name}' not found")
         exit(1)
 
-    win_status = [False] * len(boards)
-    win_num: int = -1
-    last_num: int = -1
+    win_status: list[bool] = [False] * len(boards)
+    first_num: int = -1
     first_win: np.ndarray = np.ndarray([])
+    last_num: int = -1
     last_win: np.ndarray = np.ndarray([])
     for num in order:
+        # When all boards have been won, stop reading the numbers
         if all(win_status):
             break
 
         for board_num, board in enumerate(boards):
+            # Don't bother to repeat boards that have been won already
             if win_status[board_num]:
                 continue
 
-            for i, row in enumerate(board):
-                for j, val in enumerate(row):
-                    if val == num:
-                        board[i][j] = -1
+            # Check if the board contains the current number and mark it if so
+            mark(board, num)
 
+            # Check if the board has won as a result
             if has_won(board):
-                if all(not i for i in win_status):
-                    win_num = num
-                    first_win = board
+                win_status[board_num] = True  # This board has won
 
-                win_status[board_num] = True
-                if all(win_status):
+                # This is the first board to win
+                if sum(win_status) == 1:
+                    first_win = board
+                    first_num = num
+                # This is the last board to win
+                elif all(win_status):
                     last_win = board
                     last_num = num
 
-    print(f"Part 1: {score(first_win, win_num)}")
+    print(f"Part 1: {score(first_win, first_num)}")
     print(f"Part 2: {score(last_win, last_num)}")
 
 
