1563792c5c961d72414439c2f64f060e02c95b4f
Pedro Avalos <pavalos6401@gmail.com>
2021-12-07 17:49:46 -0600

    day 7 part 2

--- day-07/fuel.py
+++ day-07/fuel.py
@@ -10,12 +10,15 @@ def main():
     with open("input.txt") as f:
         positions = [int(i) for i in f.read().strip().split(",")]
 
-    possibilities: list[int] = [0] * (max(positions) - min(positions) - 1)
-    for i in range(len(possibilities)):
+    possibilities_1: list[int] = [0] * (max(positions) - min(positions) - 1)
+    possibilities_2: list[int] = [0] * (max(positions) - min(positions) - 1)
+    for i in range(len(possibilities_1)):
         for j in positions:
-            possibilities[i] += abs(i - j)
+            possibilities_1[i] += abs(i - j)
+            possibilities_2[i] += sum([n + 1 for n in range(abs(i - j))])
 
-    print(f"Part 1: {min(possibilities)}")
+    print(f"Part 1: {min(possibilities_1)}")
+    print(f"Part 2: {min(possibilities_2)}")
 
 
 if __name__ == "__main__":
