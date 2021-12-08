5f889135ad4b1066c76c5b1f75e605efd7fd8dc7
Pedro Avalos <pavalos6401@gmail.com>
2021-12-07 21:22:22 -0600

    day 6 part 2

--- day-06/spawn.py
+++ day-06/spawn.py
@@ -8,6 +8,27 @@ Day 6 script.
 import sys
 
 
+def part_1(fish: list[int]) -> int:
+    copy: list[int] = list(fish)
+    for _ in range(80):
+        for j in range(len(copy)):
+            copy[j] -= 1
+            if copy[j] < 0:
+                copy[j] = 6
+                copy.append(8)
+    return len(copy)
+
+
+def part_2(fish: list[int]) -> int:
+    days: list[int] = [0] * 9
+    for f in fish:
+        days[f] += 1
+    for i in range(256):
+        today = i % len(days)
+        days[(today + 7) % len(days)] += days[today]
+    return sum(days)
+
+
 def main() -> None:
     name: str = "input.txt" if len(sys.argv) < 2 else sys.argv[1]
     fish: list[int] = []
@@ -18,15 +39,8 @@ def main() -> None:
         print(f"File '{name}' not found.")
         exit(1)
 
-    for _ in range(80):
-        for j in range(len(fish)):
-            fish[j] -= 1
-            if fish[j] < 0:
-                fish[j] = 6
-                fish.append(8)
-
-
-    print(len(fish))
+    print(f"Part 1: {part_1(fish)}")
+    print(f"Part 2: {part_2(fish)}")
 
 
 if __name__ == "__main__":
